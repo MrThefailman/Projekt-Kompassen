1df9eee4f4936e0915f05106f3423453abfb26a1
Mikael Zamecnik <mikael.zamecnik@gmail.com>
2017-11-09 19:33:22 +0100

    Gjorde en Menu.js och ändrade lite divs så dom är hide från start , pillar bara lite, inga stora ändringar

---
+++ WU16.Kompassen.Web/scripts/menu.js
@@ -0,0 +1,16 @@
+﻿    $(function () {
+
+    $("#courses").click(function () {
+        $("#courseDetailsPlaceholder").toggleClass("show-li");
+
+        $("#students").click(function () {
+            $("#studentListPlaceholder").toggleClass("show-li");
+
+        $("#addCourse").click(function () {
+            $("#courseListPlaceholder").toggleClass("show-li");
+
+        });
+        });
+
+    });
+    });
