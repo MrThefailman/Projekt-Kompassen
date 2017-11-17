46e96e837788bd5c2484aafb7d7d01599d2352f2
Mikael Zamecnik <mikael.zamecnik@gmail.com>
2017-11-17 09:12:42 +0100

    Inga större ändringar men jag måste bli av med min master uppladning så jag kan fetcha min branch på denna dator

--- WU16.Kompassen.Web/App_Start/BundleConfig.cs
+++ WU16.Kompassen.Web/App_Start/BundleConfig.cs
@@ -25,10 +25,11 @@ namespace WU16.Kompassen.Web
                       "~/scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                     "~/scripts/core/menu.js"));
+                     "~/scripts/core/menu.js",
+                     "~/scripts/core/app.js"));
                     // "~/scripts/core/utilities.js",
                     //  "~/scripts/core/page.js",
-                    // "~/scripts/core/app.js",
+                    
                     // "~/scripts/core/eventhandlers.js",
                     // "~/scripts/core/waiting-dialog.js"
 
