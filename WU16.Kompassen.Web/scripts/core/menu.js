08d1e9074a8ed5bf2ebf1f023f38c9d263d37c3b
Timmie Bark <timmie.bark@hotmail.com>
2017-11-22 14:07:04 +0100

    lägger till tre bilder

--- WU16.Kompassen.Web/scripts/core/menu.js
+++ WU16.Kompassen.Web/scripts/core/menu.js
@@ -1,21 +1,41 @@
 ﻿
 $(function () {
 
+    $("#start").click(function () {
+        $("#containerCourseList").hide();
+        $("#containerStudentList").hide();
+        $(".courseDetailsForm").hide();
+        $(".toggleAddToCourse").hide();
+
+    });
+
     $("#courses").click(function () {
         $("#containerCourseList").show();
         $("#containerStudentList").hide();
-        $("#courseDetailsForm").hide();
+        $(".courseDetailsForm").hide();
+        $(".toggleAddToCourse").show();
 
     });
 
     $("#students").click(function () {
         $("#containerStudentList").show();
         $("#containerCourseList").hide();
-        $("#courseDetailsForm").hide();
+        $(".courseDetailsForm").hide();
+        $(".toggleAddToCourse").show();
 
     });
 
     $(".toggleAddToCourse").click(function () {
-        $("#courseDetailsForm").toggle();
+        $(".toggleAddToCourse").hide();
+        if ($(".someClass").html() == "")
+        {
+            $(".someClass").html($(".containerCourseDetails").html());
+            $(".containerCourseDetails").html("");
+            
+        }
+        console.log($(".containerCourseDetails"));
+        $(".courseDetailsForm").show();
+
     });
+
 });
