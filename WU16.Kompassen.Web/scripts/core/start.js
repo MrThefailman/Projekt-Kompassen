0bd9b593cb24c326fd2aef8fee13d72e5cef48ca
Arnel Gajek <gaar1719@student.ju.se>
2017-11-22 14:49:06 +0100

    Uppdatering på startsidan: - Rätt student på rätt kurs - Presentation av stunder på kurser.

--- WU16.Kompassen.Web/scripts/core/start.js
+++ WU16.Kompassen.Web/scripts/core/start.js
@@ -5,21 +5,31 @@
     // .get hämtar URL:en.
     $.get(url, function (data) {
 
-        // .each går genom alla kurser som finns i SQL.
-        $.each(data, function (i, course) { 
+        // .each går genom alla kurser som finns i db enligt ovan URL.
+        $.each(data, function (i, course) {
+            
+            //Modulo går genom .list-group och fördelar dem på 3 resultat(.nameHolder) per row.
+            if (i % 3 === 0) {
+                $('#defaultPlaceholder').append('<div class="row"' + '</div>');
+            }
 
-            // Här hämtar den information om kurserna.
-            $("#defaultPlaceholder").append("<div class='col-md-4 list-group-" + i + "'><a href='#panel-collapse collapse' class='list-group-item active'>" + course.name + "<i>" + "</a><div class='list-group-item'>Kursstart " + course.term + " " + course.year + "</i>" + "</div><div class='nameHolder'></div></div>");
-            // $('.nameHolder').hide();
+            // Går in i #defaultPlaceholder, appendar "skapar" upp allt innehåll mellan " ...... ", lägger till course.name, course.term och course.year.
+            $("#defaultPlaceholder").append("<div class='col-md-4 list-group-" + i + " mt '><a href='#' class='list-group-item active'>" + course.name + "<i>" + "</a><div class='list-group-item'>Kursstart " + course.term + " " + course.year + "</i>" + "</div><div class='nameHolder-" + i + "'style='display:none'></div></div>");
 
-                    // .each går genom alla studenterna som finns i SQL.
+
+            // .each går genom alla studenter som finns i db enligt ovan URL.
             $.each(course.students, function (j, student) {
                 
-                        $('.list-group-' + i).find('.nameHolder').append('<div class="list-group-item">' + student.firstName + ' ' + student.lastName + '</div>');
-                        // Här hämtar den information om studenterna som finns med i respektive kurs.
-                        // $('#defaultPlaceholder').find('.list-group' + i).append('<div class="list-group-item">' + student.firstName + ' ' + student.lastName + '</div>');
+                    $('.list-group-' + i).find('.nameHolder-' + i).append('<div class="list-group-item">' + student.firstName + ' ' + student.lastName + '</div>');
+                    // Här hämtar den information om studenterna som finns med i respektive kurs.
+                    // $('#defaultPlaceholder').find('.list-group' + i).append('<div class="list-group-item">' + student.firstName + ' ' + student.lastName + '</div>');
 
              });
+
+            $('.list-group-' + i).find('a').on('click', function () {
+                $('.nameHolder-' + i).slideToggle('slow');
+
+            });
 
         });
 
