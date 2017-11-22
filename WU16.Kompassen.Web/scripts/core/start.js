a0a9c8942524c7ead5c6d1f7bf77691af50429ee
Arnel Gajek <gaar1719@student.ju.se>
2017-11-22 15:11:25 +0100

    Ändrat mina kommentarer/förklaringar till vad som sker i dem olika stegen.

--- WU16.Kompassen.Web/scripts/core/start.js
+++ WU16.Kompassen.Web/scripts/core/start.js
@@ -1,29 +1,28 @@
 ﻿$(document).ready(function () {
 
-    var url = "http://localhost:45959/api/courses"; // Hänvisar till vilken URL vi ska hämta informationen ifrån gällande db.
+    var url = "http://localhost:45959/api/courses"; // Hänvisar till vilken URL vi ska hämta informationen ifrån på db.
 
-    // .get hämtar URL:en.
+    // .get hämtar URL:en enligt ovan variabel.
     $.get(url, function (data) {
 
         // .each går genom alla kurser som finns i db enligt ovan URL.
         $.each(data, function (i, course) {
             
-            //Modulo går genom .list-group och fördelar dem på 3 resultat(.nameHolder) per row.
+            // Modulo letar efter en matchning, vid matchning utförs skapandet av en ny row och den hoppar ner till nästa steg och gör samma sak. (matchning = jämnt delbart med 3 kolumner).
             if (i % 3 === 0) {
                 $('#defaultPlaceholder').append('<div class="row"' + '</div>');
             }
 
-            // Går in i #defaultPlaceholder, appendar "skapar" upp allt innehåll mellan " ...... ", lägger till course.name, course.term och course.year.
+            // Går in i #defaultPlaceholder, appendar "skapar/skriver ut html-element för varje objekt" upp allt innehåll mellan " ...... ", lägger till course.name, course.term och course.year från db.
             $("#defaultPlaceholder").append("<div class='col-md-4 list-group-" + i + " mt '><a href='#' class='list-group-item active'>" + course.name + "<i>" + "</a><div class='list-group-item'>Kursstart " + course.term + " " + course.year + "</i>" + "</div><div class='nameHolder-" + i + "'style='display:none'></div></div>");
 
 
-            // .each går genom alla studenter som finns i db enligt ovan URL.
+            // .each går genom alla studenter i respektive kurs som finns i db enligt ovan URL.
             $.each(course.students, function (j, student) {
-                
-                    $('.list-group-' + i).find('.nameHolder-' + i).append('<div class="list-group-item">' + student.firstName + ' ' + student.lastName + '</div>');
-                    // Här hämtar den information om studenterna som finns med i respektive kurs.
-                    // $('#defaultPlaceholder').find('.list-group' + i).append('<div class="list-group-item">' + student.firstName + ' ' + student.lastName + '</div>');
 
+                    // Går in i .list-group- +1 och letar efter .nameHolder- +1, appendar "skapar/skriver ut html-element för varje objekt" upp allt innehåll mellan " ..... ", lägger till student.firstName och student.lastName. från db.
+                    $('.list-group-' + i).find('.nameHolder-' + i).append('<div class="list-group-item">' + student.firstName + ' ' + student.lastName + '</div>');
+                    
              });
 
             $('.list-group-' + i).find('a').on('click', function () {
