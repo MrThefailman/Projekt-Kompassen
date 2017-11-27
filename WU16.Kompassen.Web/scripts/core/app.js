eae4bd9ee60fb93838f788a16047c784d9a151fd
Mikael Zamecnik <zami1783@student.ju.se>
2017-11-27 14:55:35 +0100

    here you go

--- WU16.Kompassen.Web/scripts/core/app.js
+++ WU16.Kompassen.Web/scripts/core/app.js
@@ -33,8 +33,24 @@ $(function () {
             'url': "/api/courses",
             'data': JSON.stringify($("#courseListAddCourseForm").serializeObject()),
             'success': function (data) {
-                alert("Hej");
-                // Någon slags callback för att uppdatera sidan AJAX
+               
+                $.get(url, function (data) {
+                    $(".courseList").empty();
+                    $.each(data, function (i, course) { // .each går genom alla kurser som finns i SQL.
+                        $("#courseListTable > .courseList", function () {
+                            $(".courseList").append("<tr><td>" + course.name + "</td><td>" + course.credits + "</td><td>" + course.students.length
+                                + "</td><td>" + "<button type='button' class='btn btn-primary'name=" + "'"
+                                + course.id + "'" + ">Edit</button>" + "</td></tr>");
+
+                            $(".courseList tr td button").on('click', function () { // Edit Knappen För Kurser
+
+                                $("#courseDetailsPlaceholder").show(); // Visa Kursinformation i ett Formulär
+
+                            });
+                        });
+                    });
+                });
+                
             }
             });
           });
