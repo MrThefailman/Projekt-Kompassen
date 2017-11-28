1b6421a50dc65059317eef16956f2d1034c4c713
Mikael Zamecnik <zami1783@student.ju.se>
2017-11-28 19:37:40 +0100

    Gjort om App så den är lite lättare att följa

--- WU16.Kompassen.Web/scripts/core/app.js
+++ WU16.Kompassen.Web/scripts/core/app.js
@@ -1,75 +1,78 @@
 ﻿                
-                   
-                
-$(document).ready(function () {
-
+// Function Showing Courses
+function GetCourses() {
     $.get("/api/courses", function (data) {
 
-        $.each(data, function (i, course) { // .each går genom alla kurser som finns i SQL.
-            $("#courseListTable > .courseList", function () {
+        $.each(data, function (i, course) {
+            $("#courseListTable .courseList", function () {
                 $(".courseList").append("<tr><td>" + course.name + "</td><td>" + course.credits + "</td><td>" + course.students.length
+                    + "</td><td>" + "</td><td>" + course.year
+                    + "</td><td>" + "</td><td>" + course.term
                     + "</td><td>" + "<button type='button' class='btn btn-primary'data-id=" + "'"
                     + course.id + "'" + ">Edit</button>" + "</td></tr>");
 
-
-                $("#courseListTable > tbody > tr:nth-child(1) > td:nth-child(4) > button").click(function (e) {
-
-
-
-                    e.preventDefault();
-                    var cId = $(this).attr("data-id");
-
-                    $.ajax({
-                        type: "GET",
-                        url: "/api/courses/" + cId
-                    }).done(function (c) {
-                        console.log("Course selected: " + c.name);
-                        $("#courseListAddCourseForm :input[name='idnumber']").val(c.id);
-                        $("#courseListAddCourseForm :input[name='name']").val(c.name);
-                        $("#courseListAddCourseForm :input[name='credits']").val(c.credits);
-                        $("#courseListAddCourseForm :input[name='year']").val(c.year);
-                        $("#courseListAddCourseForm :input[name='term']").val(c.term);
-                        $("#courseListAddCourseForm :input[name='active']").val(c.active);
-                        
-                        
-
-                        $("#saveCourse").on('click', function (event) {
-
-                            event.preventDefault();
-
-                            $.ajax({
-                                headers: {
-                                    'Accept': 'application/json; charset=utf-8',
-                                    'Content-Type': 'application/json; charset=utf-8'
-                                },
-                                'type': 'POST',
-                                'url': "/api/courses",
-                                'data': JSON.stringify($("#courseListAddCourseForm").serializeObject()),
-                                'success': function (data) {
-
-                                    $.get(url, function (data) {
-                                        $(".courseList").empty();
-                                        $.each(data, function (i, course) { // .each går genom alla kurser som finns i SQL.
-                                            $("#courseListTable > .courseList", function () {
-                                                $(".courseList").append("<tr><td>" + course.name + "</td><td>" + course.credits + "</td><td>" + course.students.length
-                                                    + "</td><td>" + "<button type='button' class='btn btn-primary'name=" + "'"
-                                                    + course.id + "'" + ">Ändra</button>" + "</td></tr>");
-                                            });
-                                        });
-                                    });
-
-
-
-                                }
-                            });
-
-                        })
-
-
-                    });
-                });
             });
         });
-
     });
-})
+}
+// Function for POST
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+}; 
+
+$(document).ready(function () {                         // DOCUMENT READY STARTS HERE
+
+    // Edit Existing Courses
+    GetCourses(); {
+        $(".courseList :button[type='button']").on('Click', function (e) {
+            alert("FUNKAR");
+            e.preventDefault();
+            var cId = $(this).attr("data-id");
+            $.ajax({
+                type: "GET",
+                url: "/api/courses/" + cId
+            }).done(function (c) {
+                alert("Course: " + c.name);
+                $("#courseListAddCourseForm :input[name='id']").val(c.id);
+                $("#courseListAddCourseForm :input[name='name']").val(c.name);
+                $("#courseListAddCourseForm :input[name='credits']").val(c.credits);
+                $("#courseListAddCourseForm :input[name='year']").val(c.year);
+                $("#courseListAddCourseForm :input[name='term']").val(c.term);
+                $("#courseListAddCourseForm :input[name='active']").val(c.active);
+            });
+        });
+    }
+    // Posting New Courses
+    $("#courseListAddCourseForm :button").on('click', function (e) {
+        e.preventDefault();
+            
+        $.ajax({
+            headers: {
+                'Accept': 'application/json; charset=utf-8',
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            'type': 'POST',
+            'url': "/api/courses/",
+            'data': JSON.stringify($("#courseListAddCourseForm").serializeObject()),
+            'success': function (data) {
+                console.log(data);
+                $(".courseList").empty();
+                GetCourses();
+              
+            }
+        });
+        });
+
+});
