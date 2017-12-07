aec76685fcc64854670d471dc0b68f483aac14ca
Mikael Zamecnik <zami1783@student.ju.se>
2017-12-07 20:48:28 +0100

    BÅde Post och Form värden fungerar

--- WU16.Kompassen.Web/scripts/core/app.js
+++ WU16.Kompassen.Web/scripts/core/app.js
@@ -1,96 +1,101 @@
 ﻿
-    $(document).ready(function () {
+$(document).ready(function () {
     //GETCOURSES
     GetCourses();
-    function GetCourses() {
-    $.get("/api/courses", function (data) {
-        $.each(data, function (p, course) {
-    $("#courseListTable .courseList", function () {
-    $(".courseList").append("<tr><td>" + course.name + "</td><td>"
-    + course.credits + "</td><td>" + course.students.length
-    + "</td><td>" + course.year
-    + "</td><td>" + course.term
-    + "</td><td><button type='button' value='aktiv' class='btn btn-success'data-id='" + course.active + "'>Aktiv</button>"
-    + "</td><td>" + "<button type='button' id='editButton' class='btn btn-warning' data-id=" + "'"
-    + course.id + "'" + ">Redigera</button>" + "</td></tr>");
-
-
-    // EDIT COURSES
-
-    $("#courseListTable :button").on('click', function () {
-        //e.preventDefault();
-        $("#courseDetailsPlaceholder").show();
-        var cId = $(this).attr("data-id");
-        $("#courseDetailsForm :input[name='id']").val(cId);
-        $("#courseDetailsForm :input[name='name']").val(course.name);
-        $("#courseDetailsForm :input[name='credits']").val(course.credits);
-        $("#courseDetailsForm :input[name='year']").val(course.year);
-        $("#courseDetailsForm :input[name='term']").val(course.term);
-        $("#courseDetailsForm :input[name='active']").val(course.active);
-
-    });
-
-
-    });
-    
-    //Students in Selected Course
-    $.each(course.students, function (g, student) {
-    if (student.active === true) {
-
-    $("#courseDetailsStudentListPlaceholder").append("<br><p>" + student.firstName + student.lastName + "</p>");
-        }
-    });
-    });
-    // Posting new Courses
-    $("#courseListAddCourseForm :button").on('click', function (e) {
-    e.preventDefault();
-    $.ajax({
-    headers: {
-    'Accept': 'application/json; charset=utf-8',
-    'Content-Type': 'application/json; charset=utf-8'
-    },
-    'type': 'POST',
-    'url': "/api/courses/",
-    'data': JSON.stringify($("#courseListAddCourseForm").serializeObject()),
-    'success': function (data) {
-    console.log(data);
-    $(".courseList").empty();
-    GetCourses();
-    }
-    });
-    // Uppdate Course Button
-    $("#courseDetailsForm").click(function (e) {
-    e.preventDefault();
-    $.ajax({
-    headers: {
-    'Accept': 'application/json; charset=utf-8',
-    'Content-Type': 'application/json; charset=utf-8'
-    },
-    'type': 'POST',
-    'url': "/api/courses",
-    'data': JSON.stringify($("#courseDetailsForm").serializeObject()),
-    'success': function (data) {
-    console.log(data);
-    $(".courseList").empty();
-    GetCourses();
-    }
-    });
-    });
-    });
-    });
-    // GET för att hämta studenterna till dropdown-lista för kurs-fliken.
-    $.get("/api/students", function (data) {
-    $.each(data, function (l, student) {
-    $("#courseDetailsStudentSelectList").append("<option value=" + student.id + ">" + student.firstName + " " + student.lastName + "</option>");
-
-    $('#registerSelectedStudentButton').on('click', function () {
-    $("#courseDetailsStudentSelectList option:selected").remove();
-    $('#courseDetailsStudentListPlaceholder').append('<p>' + this.firstName + " " + this.lastName + '</p>');
-    });
-    });
-    });
     TopWindow();
+    function GetCourses() {
+        $.get("/api/courses", function (data) {
+            $.each(data, function (i, course) {
+                $("#courseListTable .courseList", function () {
+                    $(".courseList").append("<tr><td>" + course.name + "</td><td>"
+                        + course.credits + "</td><td>" + course.students.length
+                        + "</td><td>" + course.year
+                        + "</td><td>" + course.term
+                        + "</td><td><button type='button' value='aktiv' class='btn btn-success'data-id='" + course.active + "'>Aktiv</button>"
+                        + "</td><td>" + "<button type='button' id='editButton' class='btn btn-warning'data-id=" + "'"
+                        + course.id + "'" + ">Redigera</button>" + "</td></tr>");
+
+                });
+                // EDIT COURSES
+                $(document).on('click', ".courseList tr td button", function (e) {
+                    e.preventDefault(e);
+                    var cId = $(this).attr("data-id");
+                    $.ajax({
+                        type: 'GET',
+                        url: '/api/courses/' + cId
+                    }).done(function (courses) {
+                        //$("#courseDetailsStudentListPlaceholder").empty();
+                        $("#courseDetailsPlaceholder").show();
+
+                        $("#courseDetailsPlaceholder :input[name='id']").val(courses.id);
+                        $("#courseDetailsPlaceholder :input[name='name']").val(courses.name);
+                        $("#courseDetailsPlaceholder :input[name='credits']").val(courses.credits);
+                        $("#courseDetailsPlaceholder :input[name='year']").val(courses.year);
+                        $("#courseDetailsPlaceholder :input[name='term']").val(courses.term);
+                        $("#courseDetailsPlaceholder :input[name='active']").val(courses.active);
+                    });
+                });
+                //Students in Selected Course
+                $.each(course.students, function (g, student) {
+                    if (student.active === true) {
+
+                        $("#courseDetailsStudentListPlaceholder").append("<br><p>" + student.firstName + student.lastName + "</p>");
+                    }
+                });
+            });
+            TopWindow();
+                // Posting new Courses
+                $("#courseListAddCourseForm :button").on('click', function (e) {
+                    e.preventDefault();
+                    $.ajax({
+                        headers: {
+                            'Accept': 'application/json; charset=utf-8',
+                            'Content-Type': 'application/json; charset=utf-8'
+                        },
+                        'type': 'POST',
+                        'url': "/api/courses/",
+                        'data': JSON.stringify($("#courseListAddCourseForm").serializeObject()),
+                        'success': function (data) {
+                            console.log(data);
+                            $(".courseList").empty();
+                            GetCourses();
+                        }
+                    });
+                    // Uppdate Course Button
+                    $("#courseDetailsForm").submit(function (e) {
+                        e.preventDefault();
+                        $.ajax({
+                            headers: {
+                                'Accept': 'application/json; charset=utf-8',
+                                'Content-Type': 'application/json; charset=utf-8'
+                            },
+                            'type': 'POST',
+                            'url': "/api/courses",
+                            'data': JSON.stringify($("#courseDetailsForm").serializeObject()),
+                            'success': function (data) {
+                                console.log(data);
+                                $(".courseList").empty();
+                                GetCourses();
+                            }
+                        
+                    });
+                });
+            });
+        });
+        // GET för att hämta studenterna till dropdown-lista för kurs-fliken.
+        $.get("/api/students", function (data) {
+            $.each(data, function (j, student) {
+                $("#courseDetailsStudentSelectList").append("<option value=" + student.id + ">" + student.firstName + " " + student.lastName + "</option>");
+
+                $('#registerSelectedStudentButton').on('click', function () {
+                    $("#courseDetailsStudentSelectList option:selected").remove();
+                    $('#courseDetailsStudentListPlaceholder').append('<p>' + this.firstName + " " + this.lastName + '</p>');
+                });
+            });
+            TopWindow();
+        });
+        
     }
-    });
     
-    
+});
+
